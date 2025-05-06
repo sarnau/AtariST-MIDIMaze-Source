73503c5e7cf4c581b7183a21b35424cf5a850005
Markus Fritze <github@sarnau.com>
2025-05-06 18:39:53 +0200

    Removed bogus src folder

--- MIDIMaze/src/calc_tab.h
+++
@@ -1,6 +0,0 @@
-extern int	mult160table[256];
-extern int	mult80table[SCREEN_BW_HEIGHT];
-
-extern const unsigned short bmask_allDown[16];
-extern const unsigned short bmask_allUp[16];
-extern const unsigned short bmask_singlebit[16];
